00537387aceb4981f183ca8fac311009fb506231
Mangeshlawande <mangeshlawande223@gmail.com>
2025-02-11 09:53:54 +0530

    add project files to the code base

---
+++ BlogApplication/src/store/authSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// make store to track the authentication 
+
+// redux toolkit automatically handles spreading existing values ;
+//
+
+const initialState = {
+    status : false ,
+    userData :null, // userInfo
+}
+
+const authSlice  =createSlice({ 
+     name: "auth",//required components
+     initialState,
+     reducers :  // reducers an object 
+     {
+        login : (state,action) => {
+            state.status = true;
+            state.userData = action.payload.userData;
+        },
+        logout : (state) => {
+            state.status = false;
+            state.userData = null;
+            // call logout method ,it can change the state 
+        }
+
+     }
+})
+
+export const {login,logout} =  authSlice.actions;
+
+export default authSlice.reducer;  // authSlice mai se reducer export kr do 
+
+// reducer ke individual function bhi export karne padte hain , to know the state  , dispatching this methods 
+// 
